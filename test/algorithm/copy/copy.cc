4bdf898e00b7ce44c0c6ac01fe7d98b412f678d8
Sunshine <guang334419520@126.com>
2018-02-28 17:18:53 +0800

    测试文件

---
+++ test/algorithm/copy/copy.cc
@@ -0,0 +1,86 @@
+/**
+ * @Author: YangGuang <sunshine>
+ * @Date:   2018-02-27T16:51:42+08:00
+ * @Email:  guang334419520@126.com
+ * @Filename: test.cc
+ * @Last modified by:   sunshine
+ * @Last modified time: 2018-02-28T12:38:36+08:00
+ */
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <list>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::list;
+using std::deque;
+
+class C {
+public:
+  C() : data_(3) {}
+private:
+  int data_;
+};
+
+int main(int argc, char const *argv[]) {
+  // test 1
+  const char ccs[5] = {'a', 'b', 'c', 'd', 'e'};    //数据来源
+  char ccd[5];                                      //数据去处
+  std::copy(ccs, ccs + 5, ccd);
+  // 调用来源
+  // copy(const char*)
+
+  // test 2
+  const wchar_t cwcs[5] = {'a', 'b', 'c', 'd', 'e'};  //数据来源
+  wchar_t cwcd[5];                  //数据去处
+  std::copy(cwcs, cwcs + 5, cwcd);
+  //调用来源
+  // copy(wchar_t*)
+
+  int ia[5] = {0, 1, 2, 3, 4};
+  std::copy(ia, ia + 5, ia);
+  //调用来源
+  //copy()
+  //__copy_dispatch(T*, T*)
+  //__copy_t(__true_type)
+
+  list<int> ilists(ia, ia + 5);             //数据来源
+  list<int> ilistd(5);                    //数据去处
+  std::copy(ilists.begin(), ilists.end(), ilistd.begin());
+  //调用来源
+  //copy()
+  //__copy_dispatch()
+  //__copy_d(input_iterator_tag)
+
+  //此处会调用copy函数
+  vector<int> ivecs(ia, ia + 5);        //数据来源
+  vector<int> ivecd(5);               //数据去处
+  std::copy(ivecs.begin(), ivecs.end(), ivecd.begin());
+  //调用来源
+  //cipy()
+  //__copy_dispatch(T*, T*)
+  //__copy_t(__true_type)
+
+  C c[5];
+  vector<C> Cys(c, c+5);      //数据来源
+  //上面对调用会调用到copy函数
+  vector<C> Cyd(5);         //数据去处
+  std::copy(Cys.begin(), Cys.end(), Cyd.begin());
+  //调用来源
+  //copy()
+  //__copy_dispatch(T*, T*)
+  //__copy_t(__false_type)
+
+  deque<C> Cds(c, c+5);     //数据来源
+  deque<C> Cdd(5);          //数据去处
+  std::copy(Cds.begin(), Cds.end(), Cdd.begin());
+  //copy()
+  //__copy_dispatch()
+  //__copy_d(random_access_iterator_tag)
+
+  return 0;
+}
---
+++ test/vector/test.cc
@@ -0,0 +1,89 @@
+/**
+ * @Author: YangGuang <sunshine>
+ * @Date:   2018-01-31T20:19:44+08:00
+ * @Email:  guang334419520@126.com
+ * @Filename: test.cc
+ * @Last modified by:   sunshine
+ * @Last modified time: 2018-02-28T14:43:46+08:00
+ */
+#include "Vector.h"
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+//using namespace simple_stl;
+int main(int argc, char const *argv[]) {
+
+  int i;
+
+  gstl::Vector<int> iv(2, 9);
+  cout << "size = " << iv.size() << endl;
+  cout << "capacity = " << iv.capacity() << endl;
+
+  iv.push_back(1);
+  cout << "size = " << iv.size() << endl;
+  cout << "capacity = " << iv.capacity() << endl;
+
+  iv.push_back(2);
+  cout << "size = " << iv.size() << endl;
+  cout << "capacity = " << iv.capacity() << endl;
+
+  iv.push_back(3);
+  cout << "size = " << iv.size() << endl;
+  cout << "capacity = " << iv.capacity() << endl;
+
+  iv.push_back(4);
+  cout << "size = " << iv.size() << endl;
+  cout << "capacity = " << iv.capacity() << endl;
+
+  for(i = 0; i < iv.size(); i++)
+    cout << iv[i] << '\t';
+
+  cout << endl;
+
+  iv.push_back(5);
+  cout << "size = " << iv.size() << endl;
+  cout << "capacity = " << iv.capacity() << endl;
+
+  for(i = 0; i < iv.size(); i++)
+    cout << iv[i] << '\t';
+  cout << endl;
+
+  iv.pop_back();
+  iv.pop_back();
+  cout << "size = " << iv.size() << endl;
+  cout << "capacity = " << iv.capacity() << endl;
+  for(i = 0; i < iv.size(); i++)
+    cout << iv[i] << '\t';
+  cout << endl;
+
+  gstl::Vector<int>::iterator it = iv.begin();
+  iv.erase(it + 3);
+
+  for(it = iv.begin(); it != iv.end(); ++it)
+    cout << *it << '\t';
+
+  cout << endl;
+
+  for(auto &i : iv)
+    cout << i << '\t';
+
+  cout << endl;
+
+  it = iv.begin();
+  iv.insert(it + 2, 3, 7);
+  cout << "size = " << iv.size() << endl;
+  cout << "capacity = " << iv.capacity() << endl;
+
+  for(i = 0; i < iv.size(); i++)
+    cout << iv[i] << '\t';
+
+  cout << endl;
+
+  iv.clear();
+  cout << "size = " << iv.size() << endl;
+  cout << "capacity = " << iv.capacity() << endl;
+
+
+  return 0;
+}
