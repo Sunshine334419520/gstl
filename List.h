5fe6ae6cdf812674b63014cc9fe73a7e5d541221
Sunshine <guang334419520@126.com>
2018-03-05 17:44:47 +0800

    List容器完成80%

---
+++ List.h
@@ -0,0 +1,401 @@
+/**
+ * @Author: YangGuang <sunshine>
+ * @Date:   2018-03-04T15:59:40+08:00
+ * @Email:  guang334419520@126.com
+ * @Filename: List.h
+ * @Last modified by:   sunshine
+ * @Last modified time: 2018-03-05T17:41:50+08:00
+ */
+
+#ifndef GSTL_LIST_H
+#define GSTL_LIST_H
+
+#include "allocate.h"
+#include "Algorithm.h"
+#include "Uninitialized.h"
+#include "iterator.h"
+#include "Traits.h"
+#include "reverseiterator.h"
+
+__GSTL_BEGIN_NAMESPACE
+
+/* 链表节点数据结构 */
+template <class T>
+struct ListNode {
+  ListNode<T> *prev;
+  ListNode<T> *next;
+  T           data;
+};
+
+/* List 的迭代器  */
+template <class T, class Ref, class Ptr>
+class ListIterator {
+public:
+  typedef ListIterator<T, T&, T*>               iterator;
+  typedef ListIterator<T, const T&, const T*>   const_iterator;
+  typedef ListIterator<T, Ref, Ptr>             self;
+
+  typedef T                   value_type;
+  typedef Ref                 reference;
+  typedef Ptr                 pointer;
+  typedef ptrdiff_t           difference_type;
+  typedef size_t              size_type;
+  typedef ListNode<T>*        link_type;
+  typedef bidirectional_iterator_tag iterator_category;
+
+public:
+  ListIterator() : node_(nullptr) {}
+  ListIterator(const iterator& x) : node_(x.node_) {}
+  ListIterator(link_type x) : node_(x) {}
+
+  link_type* data() { return node_; }
+
+  bool operator==(const self& x) const { return this->node_ == x.node_; }
+  bool operator!=(const self& x) const { return this->node_ != x.node_; }
+
+  reference operator*() const{ return *node_->data; }
+  pointer  operator->() const { return &(operator*()); }
+
+  self& operator++() {
+    node_ = node_->next;
+    return *this;
+  }
+
+  self operator++(int) {
+    self tmp = *this;
+    ++*this;
+    return tmp;
+  }
+
+  self& operator--() {
+    node_ = node_->prev;
+    return *this;
+  }
+
+  self operator--(int) {
+    self tmp = *this;
+    --*this;
+    return tmp;
+  }
+
+private:
+  link_type node_;
+
+};
+
+//List 是一个环状链表
+template <class T, class Alloc = alloc>
+class List {
+public:
+  typedef ListNode<T>           list_node;
+  typedef SimpleAlloc<T, Alloc> list_node_alllocate;
+
+public:
+  typedef T                     value;
+  typedef value&                reference;
+  typedef value*                pointer;
+  typedef const value&          const_reference;
+  typedef const value*          const_pointer;
+  typedef size_t                size_type;
+  typedef ptrdiff_t             difference_type;
+  typedef list_node*            link_type;
+
+public:
+  typedef ListIterator<T, T&, T*>               iterator;
+  typedef ListIterator<T, const T&, const T*>   const_iterator;
+
+  typedef reverse_iterator<const_iterator>      const_reverse_iterator;
+  typedef reverse_iterator<iterator>            reverse_iterator;
+
+public:
+  List() { empty_initialize(); }
+  List(size_type n, const T& x) { fill_initialize(n, x); }
+  List(int n, const T& x) { fill_initialize(n, x); }
+  List(long n, const T& x) { fill_initialize(n, x); };
+
+  template <class InputIterator>
+  List(InputIterator first, InputIterator last) { range_initialize(first, last);}
+
+  List(const List<T, Alloc>& x) {
+    range_initialize(x.begin(), x.end());
+  }
+
+  List<T, Alloc>& operator=(const List<T, Alloc>& x);
+  List(List<T, Alloc>&& x) {
+    clear();
+    put_node(node_);
+
+    node_ = x.data();
+
+    x.data() = nullptr;
+  }
+
+  List<T, Alloc>& operator=(List<T, Alloc>&& x) {
+    if (this != &x) {
+      clear();
+      put_node(node_);
+
+      node_ = x.data();
+      x.data() = nullptr;
+    }
+    return *this;
+  }
+
+
+public:
+  iterator begin() { return node_->next; }
+  iterator end()   { return node_; }
+  const_iterator begin() const { return node_->next; }
+  const_iterator end() const { return node_; }
+  const_iterator cbegin() const { return node_->next; }
+  const_iterator cend() const { return node_; }
+  reverse_iterator rbegin() { return reverse_iterator(end()); }
+  reverse_iterator rend() { return reverse_iterator(begin()); }
+  const_reverse_iterator rcbegin() const { return const_reverse_iterator(end()); }
+  const_reverse_iterator rcend() const { return const_reverse_iterator(begin()); }
+
+  size_type size() {
+    size_typ result = distance(begin(), x.end());
+    return result;
+  }
+  size_type max_size() const { return size_type(-1); }
+  link_type* data() { return node_; }
+
+  bool empty() { return begin() == end(); }
+  reference front() { return *begin(); }
+  reference back() { return *(--end()); }
+  const_reference front() const { return *begin(): }
+  const_reference back() const { return *(--end()); }
+
+  void push_back(const T& x) { insert(end(), x); }
+  void push_front(const T& x) { insert(begin(), x); }
+
+  void pop_front() { erase(begin()); }
+  void pop_front() { iterator tmp = end(); erase(--tmp); }
+
+  iterator insert(iterator position, const T& x) {
+    link_type tmp = create_node(x);
+
+    tmp->next = position.data();
+    tmp->prev = position.data()->prev;
+    position.data()->prev->next = tmp;
+    position.data()->prev = tmp;
+
+    return tmp;
+  }
+
+  iterator insert(iterator position) { return insert(position, T()); }
+
+  iterator erase(iterator position) {
+    link_type next_node = position.data()->next;
+    link_type prev_node = position.data()->prev;
+
+    prev_node->next = next_node;
+    next_node->prev = prev_node;
+    return static_cast<iterator>(next_node);
+  }
+
+  iterator erase(iterator first, iterator last);
+
+  void clear();
+public:
+  // 将链表x移动到position之前
+  void splice(iterator position, list& x) {
+    if (!x.empty())
+      transfer(position, x.begin(), x.end());
+  }
+  // 将链表中i指向的内容移动到position之前
+  void splice(iterator position, list&, iterator i) {
+    iterator j = i;
+    ++j;
+    if(position == i || position == j) return ;
+    transfer(position, i, j);
+  }
+ // 将[first, last}元素移动到position之前
+  void splice(position, list&, iterator first, iterator last) {
+    if (first != last)
+      transfer(position, first, last);
+  }
+
+  void remove(const T& value);
+  void unique();
+  void merge(List<T, Alloc>& x);
+  void reverse();
+  void sort();
+
+
+
+
+
+
+protected:
+  //构造一个新节点，不进行构造
+  link_type get_node() const { return list_node_alllocate::Allocate(); }
+  //释放一个节点, 并不调用析构
+  void put_node(link_type p) { list_node_alllocate::Deallocate(p); }
+  //创建一个节点
+  link_type create_node(const T& x) {
+    link_type p = get_node();
+    try {
+      Construct(&(p->data), x);
+    } catch (...) {
+      put_node(p);
+    }
+
+    return tmp;
+  }
+  // 释放一个节点
+  void destroy_node(link_type p) {
+    Destroy(&(p->data));
+    put_node(p);
+  }
+protected:
+  //构造一个空链表
+  void empty_initialize() {
+    node_ = get_node();
+    node_->next = node_;
+    node->prev = node_;
+  }
+  //构造一个n个链表初始值为x的链表
+  void fill_initialize(size_type n, const T& x) {
+    empty_initialize();
+    try {
+      insert(begin(), n, x);
+    } catch(...) {
+      clear();
+      put_node(node_);
+    }
+  }
+  //构造一个区域链表
+  template <class InputIterator>
+  void range_initialize(InpuIterator first, InputIterator last) {
+    empty_initialize();
+    try {
+      insert(this->begin(), first, last);
+    } catch(...) {
+      clear();
+      put_node(node_);
+    }
+  }
+protected:
+  void transfer(iterator position, iterator first, iterator last) {
+    if (position != last) {
+      last.data()->prev->next = position.data();
+      first.data()->prev->next = last.data();
+      position.data()->prev->next = first.data();
+
+      tmp = first.data()->prev;
+      first.data()->prev = position.data()->prev;
+      position.data()->prev = last.data()->prev;
+      last.data()->prev = tmp;
+    }
+  }
+protected:
+  link_type node_;
+
+};
+
+
+template <class T, class Alloc>
+List<T, Alloc>& List<T, Alloc>::operator=(const List<T, Alloc> &x)
+{
+  if (this != &x) {
+    iterator first1 = this->begin();
+    iterator last1 = this->end();
+    iteartor first2 = x.begin();
+    iterator last2 = x.end();
+
+    //把first2的值复制给first1
+    while (first1 != last1 && first2!= last2) *first1++ = *first2++;
+    //当前元素大与x就清除多余的元素，小于插入缺少的元素
+    if (first1 == last1)
+      insert(last1, first2, last2);
+    else
+      erase(first1, last1);
+
+  }
+}
+
+template <class T, class Alloc>
+void List<T, Alloc>::clear()
+{
+  link_type cur = node_->next;
+  while (cur != end()) {
+    link_type tmp = cur;
+    cur = cur->next;
+    destroy_node(cur);
+  }
+
+  node_->next = node_;
+  node_->prev = node_;
+}
+
+template <class T, class Alloc>
+void List<T, Alloc>::remove(const T &value)
+{
+  iterator first = begin();
+  iterator last = end();
+
+  while (first != last)
+    if(*first == value)
+      first = erase(first);
+    else
+      ++first;
+}
+
+template <class T, class Alloc>
+void List<T, Alloc>::unique()
+{
+  iterator first = begin();
+  iterator last = end();
+
+  //如果为空链表，直接返回
+  if (first == last) return ;
+  iterator next = first;
+  while (++next != last) {
+    if (*first == *next)
+      erase(next);
+    else
+      first = next;
+    next = first;
+  }
+
+}
+
+template <class T, class Alloc>
+void List<T, Alloc>::merge(List<T, Alloc> &x)
+{
+  iterator first1 = begin();
+  iterator last1 = end();
+  iterator first2 = x.begin();
+  iterator last2 = x.end();
+
+  while (first1 != last1 && first2 != last2)
+    if (*first2 < *firsst1) {
+      iterator next = first2;
+      transfer(first1, first2, ++next);
+      first2 = next;
+    } else {
+      ++first;
+    }
+    if (first2 != last2) transfer(last1, first2, last2);
+}
+
+template <class T, class Alloc>
+void List<T, Alloc>::reverse()
+{
+  //空链表或者仅有一个元素，直接返回
+  if (node->next == node || node->next->next == node)
+    return ;
+  iterator first = begin();
+  ++first;
+  while (first != end()) {
+    iterator old_first = first;
+    ++first;
+    transfer(begin(), old_first, first);
+  }
+}
+
+__GSTL_END_NAMESPACE
+
+#endif
