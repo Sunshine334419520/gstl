d5ef62d4065406d31ee1f1f655a82b68ec8f0222
Sunshine <guang334419520@126.com>
2018-03-10 21:54:01 +0800

    添加一些copy fill版本

--- uninitialized.h
+++ uninitialized.h
@@ -4,13 +4,14 @@
  * @Email:  guang334419520@126.com
  * @Filename: uninitialized.h
  * @Last modified by:   sunshine
- * @Last modified time: 2018-02-28T13:35:32+08:00
+ * @Last modified time: 2018-03-10T21:04:50+08:00
  */
 
 #ifndef GSTL_UNINITIALIZED_H
 #define GSTL_UNINITIALIZED_H
 
 #include <cstring>
+#include "iterator.h"
 #include "Algorithm.h"
 
 __GSTL_BEGIN_NAMESPACE
@@ -106,7 +107,7 @@ template <class ForwardIterator, class T>
 inline void
 uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& x)
 {
-  __uninitialized_fill(first, last, x, vaule_type(first));
+  __uninitialized_fill(first, last, x, value_type(first));
 }
 
 
@@ -149,6 +150,53 @@ uninitialized_fill_n(ForwardIterator first, Size n, const T& x)
   return __uninitialized_fill_n(first, n, x, value_type(first));
 }
 
+
+
+template <class InputIterator1, class InputIterator2, class ForwardIterator>
+inline ForwardIterator
+__uninitialized_copy_copy(InputIterator1 first1, InputIterator1 last1,
+                          InputIterator2 first2, InputIterator2 last2,
+                          ForwardIterator result)
+{
+  ForwardIterator mid = uninitialized_copy(first1, last1, result);
+  try {
+    return uninitialized_copy(first2, last2, mid);
+  }
+  catch (...) { Destroy(result, mid); }
+
+  //return static_cast<ForwardIterator>(0);
+}
+
+// Fills [result, mid) with x, and copies [first, last) into
+//  [mid, mid + (last - first)).
+template <class ForwardIterator, class T, class InputIterator>
+inline ForwardIterator
+__uninitialized_fill_copy(ForwardIterator result, ForwardIterator mid,
+                          const T& x,
+                          InputIterator first, InputIterator last)
+{
+  uninitialized_fill(result, mid, x);
+  try {
+    return uninitialized_copy(first, last, mid);
+  }
+  catch (...){ Destroy(result, mid); }
+  //return static_cast<ForwardIterator>(0);
+}
+
+// Copies [first1, last1) into [first2, first2 + (last1 - first1)), and
+//  fills [first2 + (last1 - first1), last2) with x.
+template <class InputIterator, class ForwardIterator, class T>
+inline void
+__uninitialized_copy_fill(InputIterator first1, InputIterator last1,
+                          ForwardIterator first2, ForwardIterator last2,
+                          const T& x)
+{
+  ForwardIterator mid2 = uninitialized_copy(first1, last1, first2);
+  try {
+    uninitialized_fill(mid2, last2, x);
+  }
+  catch (...) { Destroy(first2, mid2); }
+}
 
 
 
