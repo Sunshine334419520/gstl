8619ce8ebd96746debaa68617eb5d3c682a83e53
Sunshine <guang334419520@126.com>
2018-03-21 17:52:59 +0800

    修改一点宏

--- config.h
+++ config.h
@@ -4,32 +4,17 @@
  * @Email:  guang334419520@126.com
  * @Filename: stl_simple_config.h
  * @Last modified by:   sunshine
- * @Last modified time: 2018-03-06T16:41:19+08:00
+ * @Last modified time: 2018-03-21T15:32:46+08:00
  */
-#ifndef GSTL_CONFIG_H
-#define GSTL_CONFIG_H
+#ifndef __GSTL_CONFIG_H
+#define __GSTL_CONFIG_H
 
-
-
-
-
-
-
-
- #define __GSTL_NULL_TMPL_ARGS <>
-
-
- #define __STL_TEMPLATE_NULL template<>
-
+#define __GSTL_NULL_TMPL_ARGS <>
+ #define __GSTL_TEMPLATE_NULL template<>
 
 # define __GSTL gstl
 # define __GSTL_BEGIN_NAMESPACE namespace gstl {
 # define __GSTL_END_NAMESPACE }
-
-
-
-
-
 
 
 # endif/* __STL_CONFIG_H */
